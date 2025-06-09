60fb22dc79608d8061a0244a9e6a3880ad3cc05b
Parth Lahigade <parthlahigade27@gmail.com>
2025-06-09 17:38:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Courses from './pages/Courses';
+import Contact from './pages/Contact';
+import Apply from './pages/Apply';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/apply" element={<Apply />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
